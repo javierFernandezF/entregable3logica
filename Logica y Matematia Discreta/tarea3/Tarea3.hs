24e3dc705cae2087b3c9c5adb45b8665893a0780
javier fernandez <jfernandez@incosa.com.uy>
2025-11-04 22:24:14 -0300

    falta parte del ultimo

--- Logica y Matematia Discreta/tarea3/Tarea3.hs
+++ Logica y Matematia Discreta/tarea3/Tarea3.hs
@@ -1,7 +1,7 @@
-import prelude
+import Prelude
 
 
-module Tarea3 where
+
 
 ---------------------------------
 --Nombre 1	: Javier Fernandez
@@ -18,15 +18,13 @@ module Tarea3 where
 
 --1)
 unir:: [a] -> [a] -> [a]
-unir l1 l2 
-    | [] l2 = l2
-    | l:l1 l2 = l:(unir l1 l2)
+unir [] l2 = l2
+unir (l:l1) l2 = l:(unir l1 l2)
 
 --2)
 producto :: [Int] -> Int
-producto l 
-    | [] = 1
-    | l1:l = l1 * producto l 
+producto [] = 1
+producto (ln:l) = ln * producto l 
 
 --3)
 {--
@@ -81,17 +79,17 @@ l*(producto l1)*(producto l2) = l*(producto l1)*(producto l2)
 --------------------
 --1)
 cumplen :: (a -> Bool) -> [a] -> [a]
-cumplen p x:xs 
-    | p [] = []
+cumplen _ [] = []
+cumplen p (x:xs) 
     | p x = x: cumplen p xs
     | otherwise = cumplen p xs
 
 --2)
 descartar :: (a -> Bool) -> [a] -> [a]
-descartar p x:xs 
-    | p [] = []
-    | p x = descartar xs
-    | otherwise = x: descartar xs
+descartar _ [] = []
+descartar p (x:xs) 
+    | p x = descartar p xs
+    | otherwise = x: descartar p xs
 
 --3)
 {--
@@ -180,26 +178,67 @@ data Tree = L Int | U Int Tree | B Tree Int Tree
  
 --1)
 t :: Tree
-t = undefined
+t = B (L 4) (8) (B (U 14 (L 9)) (16) (L 18))
 
 --2)
 sumTree :: Tree -> Int
-sumTree t = undefined 
+sumTree (L a) = a
+sumTree (U a b) = a + sumTree b
+sumTree (B a b c) = b + sumTree a + sumTree c
 
 --3)
 treeToList :: Tree -> [Int]
-treeToList t = undefined
+treeToList (L a) = [a]
+treeToList (U a b) = treeToList b ++ [a] 
+treeToList (B a b c) = treeToList a ++ [b] ++ treeToList c
+
 
 --4)
 treeHeight :: Tree -> Int
-treeHeight t = undefined
+treeHeight (L a) = 1
+treeHeight (U a b) = 1 + treeHeight b
+treeHeight (B a b c) = 1 + max (treeHeight a) (treeHeight c)
 
---5) 
+
+
+--5)
+{-- 
 completeLevel :: Tree -> Int -> Tree
-completeLevel t h = undefined
+completeLevel t h
+            | h == 1 && t == (L a) = (L a)
+            | h > 1 && t == (U a b) = B ((completeLevel b (h-1)) (a) (completeLevel b (h-1)))
+            | h > 1 && t == (L a) = B ((completeLevel (L a) (h-1)) (a) (completeLevel (L a) (h-1)))
+            | h > 1 && t == (B a b c) = B ((completeLevel a (h-1)) (b) (completeLevel c (h-1)))
+            |otherwise = t
+--}
+completeLevel :: Tree -> Int -> Tree
+completeLevel (L a) h 
+                | h == 1 = (L a)
+                | otherwise = B (completeLevel (L a) (h-1)) a (completeLevel (L a) (h-1))
+completeLevel (U a b) h = B (completeLevel b (h-1)) (a) (completeLevel b (h-1))
+completeLevel (B a b c) h = B (completeLevel a (h-1)) b (completeLevel c (h-1))
+
+
 
 --6)
 {--
-(∀t::Tree) treeHeight(completeLevels t) = treeHeight t
+(∀t::Tree) treeHeight(completeLevel t (treeHeight t)) = treeHeight t
+
+Caso base t = (L a)
+
+treeHeight(completeLevel (L a) (treeHeight (L a))) = treeHeight (L a)
+
+Por def 
+-> treeHeight L a = 1
+-> completeLevel L a 1 = L a
+-> treeHeight L a =  1
+
+Pasos:
+treeHeight(completeLevel (L a) (treeHeight (L a))) = treeHeight (L a)
+treeHeight(completeLevel La 1) = treeHeight (L a)
+treeHeight (L a) = treeHeight (L a)
+1 = 1
+
+Caso inductivo: 
 
 --}
